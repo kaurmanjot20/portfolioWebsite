c2f5421dd52c69857b7fdcb4a64ddab2b04bf8a6
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-06-01 15:27:16 +0530

    chore: Update personal card layout and project images

--- vite-portfolio/src/InfoProjects.jsx
+++ vite-portfolio/src/InfoProjects.jsx
@@ -1,4 +1,4 @@
-
+import sps from './assets/spsatl.jpeg'
 const projects = [
     {
         id: 1,
@@ -6,7 +6,7 @@ const projects = [
         description: "I initiated and created a website for the Robotics Lab of Shiwalik Public School, Rupnagar to give students a platform to display their creativity and passion towards Robotics and Technology. The website serves as a hub for sharing project updates, achievements, and fostering a community that encourages collaboration and knowledge exchange.",
         githubLink: "https://github.com/user/project-one",
         liveLink: "https://project-one.live",
-        coverImg: "mna",
+        coverImg: sps,
         techStack: ["HTML", "CSS", "JavaScript", "React"]
     },
     {
